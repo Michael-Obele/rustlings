0b1ff92c4fa0bfd573a89814307965a93803d23f
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-01-03 08:36:26 +0100

    🔄 refactor ♻️(vecs): Enhance vector manipulation functions with improved implementations and added test cases.

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,8 +4,10 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        // If the input is `[1, 2, 3]`, the output is `[2, 4, 6]`.
+        output.push(element * 2);
     }
-
+    println!("{:?}", output);
     output
 }
 
@@ -21,16 +23,39 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     // by 2, but with iterator mapping instead of manually pushing into an empty
     // vector.
     // See the example in the function `vec_map_example` above.
-    input
-        .iter()
-        .map(|element| {
-            // ???
-        })
-        .collect()
+    input.iter().map(|element| element * 2).collect()
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    let input = [2, 4, 6, 8, 10];
+    let ans = vec_loop(&input);
+    let ans2 = vec_map(&input);
+
+    fn vec_loop(input: &[i32]) -> Vec<i32> {
+        let mut output = Vec::new();
+
+        for element in input {
+            // TODO: Multiply each element in the `input` slice by 2 and push it to
+            // the `output` vector.
+            // If the input is `[1, 2, 3]`, the output is `[2, 4, 6]`.
+            output.push(element * 2);
+        }
+        println!("{:?}", output);
+        output
+    }
+
+    fn vec_map(input: &[i32]) -> Vec<i32> {
+        // TODO: Here, we also want to multiply each element in the `input` slice
+        // by 2, but with iterator mapping instead of manually pushing into an empty
+        // vector.
+        // See the example in the function `vec_map_example` above.
+        input.iter().map(|element| element * 2).collect()
+    }
+
+    println!("{:?}", ans);
+    println!("{:?}", ans2);
 }
 
 #[cfg(test)]
